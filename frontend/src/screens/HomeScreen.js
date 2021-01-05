3a1db6e6d8b256af0940f922079a1f0223e88f8b
adwaitgondhalekar <adwaitgondhalekar@gmail.com>
2021-01-06 02:49:26 +0530

    ProductList Reducer Added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,63 +1,43 @@
-import React,{ useState,useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { productListReducer } from "../reducers/ProductReducer";
+import { listProducts } from "../actions/ProductActions";
 
-function HomeScreen()
-{
+function HomeScreen() {
+  //const [products,setProducts] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
 
-    const [products,setProducts] = useState([]);
+  useEffect(() => {
+    dispatch(listProducts());
+    return () => {
+      //
+    };
+  }, []);
 
-    useEffect(() => {
-        
-        const fetchData = async() => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
-        }
-
-        fetchData();
-
-
-        return () => {
-            //
-        }
-    }, [])
-
-
-
-    return(
-        <div className='products'>
-            {
-            
-        products.map(product =>
-            <li key={product._id} className="li_list">
-            <div className='product'>
-                <div className='product-img'>
-                <img src={product.image} alt="a" className='img'></img>
-                </div>
-                <div className='product-title'>
-                {product.name}
-                </div>
-                <div className='product-price'>
-                    ₹{product.price}
-                </div>
-                <div className='product-rating'>
-                {product.rating} Stars
-                </div>
-                <div className='num-reviews'>
-                {product.numreviews} Reviews
-                </div>
-
-                
+  return loading ? (
+    <div>Loading..</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div className="products">
+      {products.map((product) => (
+        <li key={product._id} className="li_list">
+          <div className="product">
+            <div className="product-img">
+              <img src={product.image} alt="a" className="img"></img>
             </div>
-            </li>
-           
-        
-            )}
-           
-            </div>
-        
-    
-    );
-
+            <div className="product-title">{product.name}</div>
+            <div className="product-price">₹{product.price}</div>
+            <div className="product-rating">{product.rating} Stars</div>
+            <div className="num-reviews">{product.numreviews} Reviews</div>
+          </div>
+        </li>
+      ))}
+    </div>
+  );
 }
 
 export default HomeScreen;
